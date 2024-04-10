b54bafc4887042c9a0777ce56900e256b473080c
Taifinkaba <taifinkabad@gmail.com>
2024-04-10 01:18:04 -0400

    did SA Q1

--- src/playground.js
+++ src/playground.js
@@ -10,3 +10,16 @@
 //   getAllYCoordinates,
 // } = require('./from-scratch');
 // const { uppercaseAll, destructureCoordinates } = require('./modify');
+
+let name = "mike";
+const changeName = (nameToChange) => {
+  nameToChange = "tom";
+};
+changeName(name);
+console.log(name);
+const favoriteLocations = ["New York City", "Jersey City"];
+const addBostonToFavorites = (locations) => {
+  locations.unshift("Boston");
+};
+addBostonToFavorites(favoriteLocations);
+console.log(favoriteLocations);
