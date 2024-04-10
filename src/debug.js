7beca1ec3e5de59c83cf9855b4516a4b1ca96d98
Taifinkaba <taifinkabad@gmail.com>
2024-04-09 20:24:04 -0400

    Q11 done

--- src/debug.js
+++ src/debug.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-param-reassign */
 const clearArr = (arr) => {
-  arr = [];
+  for(let i = arr.length - 1; i >= 0; i--){
+    arr.splice(i, 1);
+  }
 };
 
 const getFirstItem = (array) => {
